b44392e3a325d2809d8255662216792c574ba00f
Fangchen Chen <chenfc@live.unc.edu>
2021-11-26 16:26:08 -0500

    3

--- src/mild/mild_1.js
+++ src/mild/mild_1.js
@@ -39,8 +39,8 @@ export function getIncreasingArray(startNumber, endNumber) {
  * and https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
  */
 export function maxAndMin(numbers) {
-    let mymin = Math.min(numbers);
-    let mymax = Math.max(numbers);
+    let mymin = Math.min(...numbers);
+    let mymax = Math.max(...numbers);
     let rtn = {min:mymin, max:mymax};
     return rtn;
 }
@@ -56,5 +56,9 @@ export function maxAndMin(numbers) {
  *
  */
 export function countArray(array) {
-
+    let rtn={}
+    for (const obj of array){
+        rtn[obj] = rtn[obj] ? rtn[obj]+1 :1;
+    }
+    return rtn;
 }
