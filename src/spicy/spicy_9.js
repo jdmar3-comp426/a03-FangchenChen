4297f6a4244a63bf716978da8ce5b93c2f8b759c
Fangchen Chen <chenfc@live.unc.edu>
2021-11-26 19:20:57 -0500

    final

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -175,7 +175,7 @@ export const filter = (arr, test) => {
  *   odd numbers. Use the "everyEven" function in this function.
  */
 export const allEvensAreOdd = (arr) => {
-
+    return everyEven(arr, x => x%2==1);
 };
 
 
@@ -185,7 +185,7 @@ export const allEvensAreOdd = (arr) => {
  *   array is an odd number. Use the "someEven" function in this function.
  */
 export const anEvenIsOdd = (arr) => {
-
+    return someEven(arr, x => x%2==1);
 };
 
 
@@ -196,5 +196,11 @@ export const anEvenIsOdd = (arr) => {
  *   pass the test. You must use the filter function.
  */
 export const hasExactly = (arr, test, n) => {
-
+    var result = filter(arr,test);
+    if (result.pass.length==n){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
